6f00873dba109704ab3289210e99bbbe5f3f6fdb
andrewpoock <66223719+andrewpoock@users.noreply.github.com>
2020-06-11 15:40:24 -0500

    Add files via upload

--- test_validation_with_try.py
+++ test_validation_with_try.py
@@ -5,7 +5,7 @@ from input_validation import validation_with_try
 class FunctionTestCase(unittest.TestCase):
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
-            validation_with_try.average(90, -89, 78)
+            validation_with_try.average(90, 89, -78)
 
 
 if __name__ == '__main__':
